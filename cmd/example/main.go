3553a34ea33bc990adc6c3a2c2522736e0be0dfd
fatsummercat <homelnw77@gmail.com>
2025-12-22 23:00:39 +0700

    feat: add edge cases

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/shopspring/decimal"
 	"github.com/studysoros/go-thaicurrency"
@@ -13,21 +14,32 @@ func main() {
 		decimal.NewFromFloat(33333.75),
 		decimal.NewFromFloat(01),
 		decimal.NewFromFloat(.01),
+		decimal.NewFromFloat(.2),
+		decimal.NewFromFloat(.02),
 		decimal.NewFromFloat(.11),
 		decimal.NewFromFloat(1234567891231.21),
 		decimal.NewFromFloat(111.55),
 		decimal.NewFromFloat(111.550),
 		decimal.NewFromFloat(111.555),
 		decimal.NewFromFloat(111.5),
+		decimal.NewFromFloat(5.461),
+		decimal.NewFromFloat(5.465),
+		decimal.NewFromFloat(0),
+		decimal.NewFromInt(math.MaxInt64), // 9223372036854775807
+
+		decimal.RequireFromString("9223372036854775808"),
+		decimal.RequireFromString("9223372036854775807.01"),
+		decimal.NewFromInt(math.MaxInt64).Add(decimal.NewFromInt(1)),
+		decimal.NewFromFloat(9223372036854775808),
 	}
 	for _, input := range inputs {
 		fmt.Println(input)
 		// convert decimal to thai text (baht) and print the result here
 		result, err := thaicurrency.Decimal(input)
 		if err != nil {
-			fmt.Printf("Error converting %v: %v\n", input, err)
+			fmt.Printf("Error converting %v: %v\n\n", input, err)
 			continue
 		}
-		fmt.Println(result)
+		fmt.Printf("%v\n\n", result)
 	}
 }
